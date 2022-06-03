6de062d404bad24d840a8588066e16a238c855c4
Nodir Qutlimurotov <nodirqutlimurotov@gmail.com>
2022-06-03 11:55:15 +0500

    gitNI Ulash

---
+++ script.js
@@ -0,0 +1,7 @@
+// const IconBurger = document.querySelector('.icon__burger');
+// const headerEl = document.getElementById('#header');
+// const mainEl = document.getElementById('#main');
+
+// IconBurger.addEventListener('click', () => {
+
+// })
